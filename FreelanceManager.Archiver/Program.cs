e1368f115783e1978fb859422dfd9996c1538d2c
Vitalii Koshura <delionkur-lestat@mail.ru>
2015-12-04 20:29:33 +0200

    Add FreelanceManager.Archiver utility which can be run from WIndows Scheduler

---
+++ FreelanceManager.Archiver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+using FreelanceManager.Database;
+using FreelanceManager.Properties;
+using FreelanceManager.Utilities;
+
+namespace FreelanceManager.Archiver
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var source = AppDomain.CurrentDomain.FriendlyName;
+      if (!EventLog.SourceExists(source))
+        EventLog.CreateEventSource(source, "Application");
+      EventLog.WriteEntry(source, "Starting...", EventLogEntryType.Information);
+      var db = new fmDB(true);
+      var prop = new fmProperties();
+      prop.Load(false);
+      db.setProperties(prop);
+      if (db.Connect())
+        fmUtilities.Archive(db, prop);
+      else
+        EventLog.WriteEntry(source, "Couldn't connect to database", EventLogEntryType.Error);
+    }
+  }
+}
